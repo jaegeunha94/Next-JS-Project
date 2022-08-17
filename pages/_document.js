bdce5543abd03e0a312638325b74515842cc3cb9
jaegeunha94 <jaegeunha94@gmail.com>
2022-08-17 09:51:04 +0900

    Add _document.js Test Code

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+// pages/_document.tsx
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
