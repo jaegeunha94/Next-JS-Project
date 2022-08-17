dcc3a285e2ef2c18dad78a18192fcda2890299ae
jaegeunha94 <jaegeunha94@gmail.com>
2022-08-17 10:39:37 +0900

    Add GetStaticProps TestCode

---
+++ pages/StaticProps/static.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import axios from 'axios';
+import Seo from '../../components/Seo';
+
+export default function Home({ data }) {
+  return (
+    <div className="container">
+      <Seo title="ServerProps" />
+      {data?.map((user) => (
+        <div>{user.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/users`
+  );
+  return {
+    props: { data },
+    // revalidate 속성을 사용해 해당 주기마다 파일 업데이트를 검사할 수 있음.
+    revalidate: 15, // (서버시간 기준 15초마다 db.json의 갱신여부를 검사함.
+  };
+}
