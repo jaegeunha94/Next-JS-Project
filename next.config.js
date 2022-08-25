d069fbea93434cd299151a0bfb4bdcfa36d1bd78
jaegeunha94 <jaegeunha94@gmail.com>
2022-08-25 15:04:59 +0900

    Add NextJS Test Code

--- next.config.js
+++ next.config.js
@@ -11,12 +11,44 @@ module.exports = {
       },
     ];
   },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/movies',
+  //       // destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+  //       destination: `/api/movies1/2/3`,
+  //     },
+  //   ];
+  // },
+
   async rewrites() {
-    return [
-      {
-        source: '/api/movies',
-        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
-      },
-    ];
+    return {
+      beforeFiles: [
+        // These rewrites are checked after headers/redirects
+        // and before all files including _next/public files which
+        // allows overriding page files
+        {
+          source: '/movies',
+          destination: '/movies/2',
+          has: [{ type: 'query', key: 'overrideMe' }],
+        },
+      ],
+      afterFiles: [
+        // These rewrites are checked after pages/public files
+        // are checked but before dynamic routes
+        {
+          source: '/movies/2',
+          destination: '/movies/2',
+        },
+      ],
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: '/movie/:path*',
+          destination: `/movies/:path*`,
+        },
+      ],
+    };
   },
 };
